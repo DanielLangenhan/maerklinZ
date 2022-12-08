64ccb17f2d3595487fc49293fee555ddbb759328
Daniel Langenhan <daniel_langenhan@yahoo.com.au>
2022-12-08 16:34:21 +0100

    Update weiche.ino

--- weiche.ino
+++ weiche.ino
@@ -9,21 +9,19 @@ void setup() {
   pinMode(weiche1, OUTPUT);
   pinMode(button, INPUT_PULLUP);
   digitalWrite(floodRelay, LOW); //set initial power State
-  digitalWrite(weiche1, LOW);  //set initial power State
 }
 
 void loop() {
   if(digitalRead(button)==LOW){
     if (direction==true){
-      digitalWrite(floodRelay, HIGH);
       digitalWrite(weiche1, LOW);
+      digitalWrite(floodRelay, HIGH);
       delay(200);
       digitalWrite(floodRelay, LOW);
-      digitalWrite(weiche1, HIGH);
       direction=false;
     } else  {
-      digitalWrite(floodRelay, HIGH);
       digitalWrite(weiche1, HIGH);
+      digitalWrite(floodRelay, HIGH);
       delay(200);
       digitalWrite(floodRelay, LOW);
       direction=true;
