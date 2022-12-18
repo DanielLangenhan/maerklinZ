90aaa32bd9c98599ebd676ac44a3cbf9018c2477
Daniel Langenhan <daniel_langenhan@yahoo.com.au>
2022-12-18 20:35:36 +0100

    switchTrack inside the loop debounced

---
+++ control switchTrackLoop.ino
@@ -0,0 +1,40 @@
+//switch
+volatile int forward=2;
+volatile int backward=3;
+
+//---------------- Functions 
+void interRed(){
+  static unsigned long lastRed=0;
+  unsigned long currentRed = millis();
+  if (currentRed - lastRed >200){
+    Serial.println("forward");
+  }
+  lastRed=currentRed;
+}
+void interGreen(){
+  static unsigned long lastGreen=0;
+  unsigned long currentGreen = millis();
+  if (currentGreen - lastGreen >200){
+    Serial.println("backward");
+    }
+  lastGreen=currentGreen;
+}
+//---------------- Functions 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  //SWITCH
+  pinMode(forward, INPUT_PULLUP);
+  pinMode(backward, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(forward)==LOW){
+    interRed();
+  }
+  if (digitalRead(backward)==LOW){
+    interGreen();
+  }
+}
